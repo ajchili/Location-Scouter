df9459125af3afe33619bb605b612b7833c8353e
Kirin Patel <kirinpatel@gmail.com>
2019-01-10 18:18:36 -0500

    Added CreateNewDialog component

---
+++ src/CreateNewDialog/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import {
+  Avatar,
+  Dialog,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText
+} from "@material-ui/core";
+import CategoryIcon from "@material-ui/icons/Category";
+import LocationIcon from "@material-ui/icons/PinDrop";
+import Proptypes from "prop-types";
+
+class CreateNewDialog extends Component {
+  _onClose = () => {
+    this.props.onClose();
+  };
+
+  _handleListItemClicked = value => {
+    this.props.onClose(value);
+  };
+
+  render() {
+    const { onClose, ...other } = this.props;
+
+    return (
+      <Dialog onClose={this._onClose} {...other}>
+        <DialogTitle>Create New...</DialogTitle>
+        <List>
+          <ListItem
+            button
+            onClick={() => this._handleListItemClicked("category")}
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <CategoryIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={"Category"} />
+          </ListItem>
+          <ListItem
+            button
+            onClick={() => this._handleListItemClicked("location")}
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <LocationIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={"Location"} />
+          </ListItem>
+        </List>
+      </Dialog>
+    );
+  }
+}
+
+CreateNewDialog.propTypes = {
+  onClose: Proptypes.func
+};
+
+export default CreateNewDialog;
--- src/LocationsList/index.js
+++ src/LocationsList/index.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import { AppBar, Fab, Toolbar, Typography } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
+import CreateNewDialog from "../CreateNewDialog";
 
 class LocationList extends Component {
+  state = {
+    createNewDialogOpen: false
+  };
+
+  _onCreateNewDialogClose = value => {
+    this.setState({ createNewDialogOpen: false });
+    // TODO: Handle value
+  };
+
   render() {
+    const { createNewDialogOpen } = this.state;
+
     return (
       <div style={styles.content}>
+        <CreateNewDialog
+          open={createNewDialogOpen}
+          onClose={this._onCreateNewDialogClose}
+        />
         <AppBar position="fixed" color="default" style={styles.AppBar}>
           <Toolbar>
             <Typography variant="h6" color="inherit" className="grow">
               Locations
             </Typography>
-            <Fab color="secondary" aria-label="add" style={styles.fabButton}>
+            <Fab
+              color="secondary"
+              aria-label="add"
+              style={styles.fabButton}
+              onClick={() => this.setState({ createNewDialogOpen: true })}
+            >
               <AddIcon />
             </Fab>
           </Toolbar>
