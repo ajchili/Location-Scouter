be6af3d01252497bc61e6b84b928b4923e983c02
Kirin Patel <kirinpatel@gmail.com>
2020-09-22 22:48:42 -0400

    Created cloud function to obtain api key

---
+++ functions/src/RequestHandlers/APIKeyFetcher.ts
@@ -0,0 +1,104 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+interface Account {
+  tier: AccountTier;
+  type: AccountType;
+}
+type AccountTier = 'admin' | 'sponsored' | 'plus' | 'basic' | 'none';
+type AccountType = 'admin' | 'user' | 'unauthenticated';
+type APIKeyName = 'map' | 'streetview';
+
+const API_QUOTA_LIMITS: Record<APIKeyName, Record<AccountTier, number>> = {
+  map: {
+    admin: Number.MAX_SAFE_INTEGER,
+    sponsored: Number.MAX_SAFE_INTEGER,
+    plus: 1000,
+    basic: 250,
+    none: 0,
+  },
+  streetview: {
+    admin: Number.MAX_SAFE_INTEGER,
+    sponsored: Number.MAX_SAFE_INTEGER,
+    plus: 500,
+    basic: 200,
+    none: 0,
+  },
+};
+
+export const handler = functions.https.onCall(
+  async (
+    data: { api: APIKeyName | undefined },
+    context: functions.https.CallableContext
+  ) => {
+    const { auth } = context;
+    if (auth === undefined) {
+      throw new functions.https.HttpsError(
+        'unauthenticated',
+        'The function must be called while authenticated!'
+      );
+    }
+    const { api } = data;
+    if (api === undefined || API_QUOTA_LIMITS[api] === undefined) {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'The function must be called with one arguments "api" containing a valid api name.'
+      );
+    }
+    const { uid } = auth;
+    const date = new Date();
+    const userDoc = await admin
+      .firestore()
+      .collection('accounts')
+      .doc(uid)
+      .get();
+    if (!userDoc.exists) {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        'Account not authorized to access resource!'
+      );
+    }
+    const { tier = 'none', type = 'unauthenticated' } =
+      (userDoc.data() as Account) || {};
+    if (tier === 'none' || type === 'unauthenticated') {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        'Account not authorized to access resource!'
+      );
+    }
+    const quotaPeriod = `${data.api}#${date.getFullYear()}-${date.getMonth()}`;
+    const docReference = admin.firestore().collection('quotas').doc(uid);
+    const usage = await admin.firestore().runTransaction(
+      async (transaction: admin.firestore.Transaction): Promise<number> => {
+        const doc = await transaction.get(docReference);
+        if (!doc.exists) {
+          transaction.set(docReference, { [quotaPeriod]: 1 });
+          return 1;
+        } else {
+          const docData = doc.data() || {};
+          const currentQuotaUsage = isNaN(docData[quotaPeriod])
+            ? 1
+            : docData[quotaPeriod];
+          if (currentQuotaUsage === API_QUOTA_LIMITS[api][tier]) {
+            return currentQuotaUsage;
+          }
+          const newQuotaUsage = currentQuotaUsage + 1;
+          transaction.update(docReference, { [quotaPeriod]: newQuotaUsage });
+          return newQuotaUsage;
+        }
+      }
+    );
+    if (usage >= API_QUOTA_LIMITS[api][tier]) {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        'Account has exceeded api usage!'
+      );
+    } else {
+      return {
+        key: functions.config().google_maps.key,
+        usage,
+        quotaPeriod,
+      };
+    }
+  }
+);
